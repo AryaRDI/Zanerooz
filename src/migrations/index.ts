fc4cc560040e4c6b318559dbdaff03cb74de7885
Arya <roudi.aria@gmail.com>
2025-12-31 18:43:07 +0100

    fix: create migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251231_174116 from './20251231_174116';
+
+export const migrations = [
+  {
+    up: migration_20251231_174116.up,
+    down: migration_20251231_174116.down,
+    name: '20251231_174116'
+  },
+];
